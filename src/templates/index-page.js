0f73860b83324db9699e1dc365e2a7858ecb1e57
Joshua Grimmett <jooshuagrimmett@gmail.com>
2021-04-16 18:45:45 +1000

    Added Image & content

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -2,26 +2,44 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link, graphql } from 'gatsby'
 
+import Img from 'gatsby-image'
+
 import Layout from '../components/Layout'
 
 export const IndexPageTemplate = ({
-  // image,
-  title,
-  heading,
-  subheading
+    image,
+    title,
+  	heading,
+  	subheading,
+	cta
 }) => (
-  <div>
-    <h1>{title}</h1>
-    <h2>{heading}</h2>
-    <h3>{subheading}</h3>
-  </div>
+  	<main
+    	role="main" 
+    	className="container row"
+	>
+      	<div
+		  className="col-sm"
+		>
+			<h2>{heading}</h2>
+      		<h3>{subheading}</h3>
+			<Link to="/slug">
+				<button className="front-cta">{cta}</button>
+			</Link>
+		</div>
+		<div
+		  className="col-sm"
+		>
+			<Img fixed={image.childImageSharp.fixed} alt="Blaze"/>
+		</div>
+  </main>
 )
 
 IndexPageTemplate.propTypes = {
-  // image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+  image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
   title: PropTypes.string,
   heading: PropTypes.string,
-  subheading: PropTypes.string
+  subheading: PropTypes.string,
+  cta: PropTypes.string
 }
 
 const IndexPage = ({ data }) => {
@@ -31,9 +49,10 @@ const IndexPage = ({ data }) => {
     <Layout>
       <IndexPageTemplate
         image={frontmatter.image}
-        // title={frontmatter.title}
+        title={frontmatter.title}
         heading={frontmatter.heading}
         subheading={frontmatter.subheading}
+		cta={frontmatter.cta}
       />
     </Layout>
   )
@@ -53,20 +72,18 @@ export const pageQuery = graphql`
   query IndexPageTemplate {
     markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
       frontmatter {
-        title
+		image {
+          childImageSharp {
+            fixed(width: 2048, quality: 100) {
+              ...GatsbyImageSharpFixed
+            }
+          }
+        }
+		title
         heading
         subheading
+		cta
       }
     }
   }
 `
-
-/*
-image {
-          childImageSharp {
-            fluid(maxWidth: 2048, quality: 100) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-        */
