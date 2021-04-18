344a871b60945b2a2c1d466b25fa6ce6abc8ab60
Joshua Grimmett <jooshuagrimmett@gmail.com>
2021-04-18 17:07:47 +1000

    Blank pages

--- src/pages/index.md
+++ src/pages/index.md
@@ -1,8 +1,7 @@
 ---
 templateKey: index-page
-title: Blaze Magic
-image: /img/079a5221.png
-heading: Blaze da mad magic man
-subheading: Yea bra he fo real yo!
-cta: Discover magic
+title: A Mongoose Introduction
+# image: 
+heading: This is a mongoose heading
+subheading: This is a mongoose subheading
 ---
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -2,44 +2,42 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link, graphql } from 'gatsby'
 
-import Img from 'gatsby-image'
 
 import Layout from '../components/Layout'
 
 export const IndexPageTemplate = ({
-    image,
-    title,
-  	heading,
-  	subheading,
-	cta
+  title,
+  heading,
+  subheading,
 }) => (
   	<main
     	role="main" 
-    	className="container row"
-	>
-      	<div
-		  className="col-sm"
-		>
-			<h2>{heading}</h2>
+    	className="container">
+      	<div>
+			<h1>{heading}</h1>
       		<h3>{subheading}</h3>
-			<Link to="/slug">
-				<button className="front-cta">{cta}</button>
-			</Link>
-		</div>
-		<div
-		  className="col-sm"
-		>
-			<Img fixed={image.childImageSharp.fixed} alt="Blaze"/>
+			<hr/>
+
+			<h2>Books</h2>
+			<ul>
+				<li>Book 1</li>
+				<li>Book 2</li>
+				<li>Book 3</li>
+			</ul>
+			
+			<h2>Charts</h2>
+			<ul>
+				<li>Chart 1</li>
+				<li>Chart 2</li>
+			</ul>
 		</div>
   </main>
 )
 
 IndexPageTemplate.propTypes = {
-  image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
   title: PropTypes.string,
   heading: PropTypes.string,
   subheading: PropTypes.string,
-  cta: PropTypes.string
 }
 
 const IndexPage = ({ data }) => {
@@ -48,11 +46,9 @@ const IndexPage = ({ data }) => {
   return (
     <Layout>
       <IndexPageTemplate
-        image={frontmatter.image}
         title={frontmatter.title}
         heading={frontmatter.heading}
         subheading={frontmatter.subheading}
-		cta={frontmatter.cta}
       />
     </Layout>
   )
@@ -71,18 +67,10 @@ export default IndexPage
 export const pageQuery = graphql`
   query IndexPageTemplate {
     markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
-      frontmatter {
-		image {
-          childImageSharp {
-            fixed(width: 2048, quality: 100) {
-              ...GatsbyImageSharpFixed
-            }
-          }
-        }
-		title
+      frontmatter { 
+		    title
         heading
         subheading
-		cta
       }
     }
   }
