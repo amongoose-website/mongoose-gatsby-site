beb292defbdbff471b96a02bc8281c8b0ea558c7
Joshua Grimmett <jooshuagrimmett@gmail.com>
2021-05-06 18:15:49 +1000

    Minimized unused code in page templates

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Link, graphql } from 'gatsby'
+import { graphql } from 'gatsby'
 
 
 import Layout from '../components/Layout'
@@ -10,28 +10,13 @@ export const IndexPageTemplate = ({
   heading,
   subheading,
 }) => (
-  	<main
-    	role="main" 
-    	className="container">
-      	<div>
-			<h1>{heading}</h1>
-      		<h3>{subheading}</h3>
-			<hr/>
+  	<main role="main">
+      <div className="container">
+        <div className="columns">
 
-			<h2>Books</h2>
-			<ul>
-				<li>Book 1</li>
-				<li>Book 2</li>
-				<li>Book 3</li>
-			</ul>
-			
-			<h2>Charts</h2>
-			<ul>
-				<li>Chart 1</li>
-				<li>Chart 2</li>
-			</ul>
-		</div>
-  </main>
+        </div>  
+      </div>
+    </main>
 )
 
 IndexPageTemplate.propTypes = {
@@ -69,8 +54,6 @@ export const pageQuery = graphql`
     markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
       frontmatter { 
 		    title
-        heading
-        subheading
       }
     }
   }
