bca1917d34c75be4b6092222f398110f866902d2
Joshua <joshua@grimmett.com.au>
2021-10-09 23:22:21 +1000

    Created index-page with inline pdf

---
+++ src/templates/index-page.js
@@ -0,0 +1,80 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { graphql } from 'gatsby'
+
+import Layout from '../components/Layout'
+import InlinePDF from '../components/PDF/InlinePDF'
+import '../components/PDF/PDF.css'
+
+export const IndexPageTemplate = ({
+  title,
+  pdf,
+  content
+}) => {
+    const metaData = { id: pdf.id, fileName: title }
+
+    return (
+        <>
+            <div id="root">
+                <div className="pageHeading">
+                  <span className="preheading">{content.preheading}</span>
+                  <span className="heading">{content.heading}</span>
+                  <span className="subheading">{content.subheading}</span>
+                </div>
+                <InlinePDF pdf={pdf.publicURL} metaData={metaData}></InlinePDF>
+            </div>
+            <script 
+                type="text/javascript" 
+                src="https://documentcloud.adobe.com/view-sdk/main.js"/>
+        </>
+    )
+}
+
+IndexPageTemplate.propTypes = {
+  title: PropTypes.string,
+  pdf: PropTypes.object,
+}
+
+const IndexPage = ({ data }) => {
+  const { frontmatter } = data.markdownRemark
+  return (
+    <Layout
+      pageTitle={frontmatter.title}>
+      <IndexPageTemplate
+        content={frontmatter.content}
+        title={frontmatter.title}
+        // body={data.markdownRemark.html}
+        pdf={frontmatter.pdf}
+      />
+    </Layout>
+  )
+}
+
+IndexPage.propTypes = {
+    data: PropTypes.shape({
+      markdownRemark: PropTypes.object,
+    }),
+  }
+
+export default IndexPage
+
+export const pageQuery = graphql`
+  query IndexPage($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      id
+      frontmatter {
+        title
+        content {
+          preheading
+          heading
+          subheading
+        }
+        pdf {
+          publicURL
+          id
+          name
+        }
+      }
+    }
+  }
+`
