2b9012d6ee1364f26e9e48edac05d7f821ce3c63
Joshua <joshua@grimmett.com.au>
2021-10-23 19:08:02 +1000

    Linked image and button to choice pages

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
+import { Link } from 'gatsby'
+
 import Icon from './Icon'
 import Img from "gatsby-image"
-import LightBoxPDF from './PDF/LightBoxPDF'
+import Button from './Button'
 
 const BookCard = ({ data }) => {	
     const { frontmatter } = data;
@@ -11,24 +13,28 @@ const BookCard = ({ data }) => {
 	return (
         <div className="book-card">
             { (data.frontmatter.content.image) &&
-                <Img className="book-thumbnail" fluid={data.frontmatter.content.image.childImageSharp.fluid}/>
+                <Link to={data.fields.slug}>
+                    <Img className="book-thumbnail" fluid={data.frontmatter.content.image.childImageSharp.fluid}/>
+                </Link>
             }
             <div className="card-info">
                 <span className="heading-text">{data.frontmatter.content.heading} - {data.frontmatter.content.subheading}</span>
                 <p className="description">
                     {data.frontmatter.content.description}
                 </p>
+                <div className="flex-end">
+                    <Button link={data.fields.slug} text="Read now"></Button>
+                    <button className="secondaryButton button__icon">
+                        <Icon name="file_download"/>
+                        Download PDF
+                    </button>
+                    <button className="secondaryButton button__icon">
+                        <Icon name="print"/>
+                        Print PDF
+                    </button>
+                </div>
             </div>
-            {/* <div className="container flex-end">
-                <LightBoxPDF pdf={data.frontmatter.pdf.publicURL} metaData={metaData}></LightBoxPDF>
-                <button className="secondaryButton button__icon">
-                    <Icon name="file_download"/>
-                    Download PDF
-                </button>
-                <button className="secondaryButton button__icon">
-                    <Icon name="print"/>
-                </button>
-            </div> */}
+            
         </div>
     )
 }
