a159537eff8c29ef78885cb6ac68507867975d3f
Joshua <joshua@grimmett.com.au>
2021-11-08 16:03:37 +1000

    Created footer for Blog

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,24 @@
 import React from 'react'
 // import { Link } from 'gatsby'
 
-// import logo from '../img/logo.svg'
-// import facebook from '../img/social/facebook.svg'
-// import instagram from '../img/social/instagram.svg'
-// import twitter from '../img/social/twitter.svg'
-// import vimeo from '../img/social/vimeo.svg'
-
 const Footer = () => {
   return (
       <footer>
-        
       </footer>
   )
 }
 
 export default Footer
+
+export const BlogFooter = ({ seriesTitle, seriesDescription }) => {
+  return (
+    <section className="blog__footer">
+      <div className="container">
+        <div className="series-definition">
+          <h2 className="series-title">{seriesTitle}</h2>
+          <span className="series-description">{seriesDescription}</span>
+        </div>
+      </div>
+    </section>
+  )
+}
