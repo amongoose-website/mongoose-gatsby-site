7f3c9a1683f50d5660ac61ced2863c14b01f0cc9
Joshua <joshua@grimmett.com.au>
2021-11-10 16:52:43 +1000

    Created new subscribe form with CSS

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
-import React from 'react'
-// import { Link } from 'gatsby'
+import React, { useState } from 'react'
+import axios from 'axios';
 
 const Footer = () => {
   return (
@@ -11,12 +11,35 @@ const Footer = () => {
 export default Footer
 
 export const BlogFooter = ({ seriesTitle, seriesDescription }) => {
+  // Subscription management
+  const [subbed, setSubbed] = useState(false);
+  const [subStatus, setSubStatus] = useState('Subscribe');
+
+  const handleSubmit = (event) => {
+    setSubStatus('Subscribing...');
+    event.preventDefault();
+  }
+
   return (
     <section className="blog__footer">
       <div className="container">
         <div className="series-definition">
           <h2 className="series-title">{seriesTitle}</h2>
           <span className="series-description">{seriesDescription}</span>
+        </div>
+        <div className="subscribeForm">
+          <h3>Get updates via email</h3>
+          {
+            !subbed && <form onSubmit={handleSubmit}>
+              <input type="email" name="email" placeholder="Email Address"></input>
+              <input type="submit" value={subStatus}></input>
+            </form>
+          }
+          {
+            subbed && <div className="subscribeForm--success">
+              <span>Subscribed</span>
+            </div>
+          }
         </div>
       </div>
     </section>
