df7e494bd4f917f5bb02e85e1f9f119938a8eaa1
Joshua <joshua@grimmett.com.au>
2022-01-04 18:38:55 +1000

    Removed unused media libraries

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,12 +1,7 @@
 import CMS from 'netlify-cms-app'
-import uploadcare from 'netlify-cms-media-library-uploadcare'
-import cloudinary from 'netlify-cms-media-library-cloudinary'
 
 import PDFPagePreview from './preview-templates/PDFPagePreview'
 import PostPagePreview from './preview-templates/PostPagePreview'
 
-CMS.registerMediaLibrary(uploadcare)
-CMS.registerMediaLibrary(cloudinary)
-
 CMS.registerPreviewTemplate('pdf-page', PDFPagePreview)
-CMS.registerPreviewTemplate('post-page', PostPagePreview)
+CMS.registerPreviewTemplate('post-pages', PostPagePreview)
