86b4060294bfe31d2eda97f54d981e4427f0a3b6
Joshua <joshua@grimmett.com.au>
2022-01-05 07:46:39 +1000

    Hooked up subscribe button to emailer api

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React from 'react'
+import axios from 'axios'
 
 const Footer = () => {
   return (
@@ -9,38 +10,68 @@ const Footer = () => {
 
 export default Footer
 
-export const BlogFooter = ({ seriesTitle, seriesDescription }) => {
-  // Subscription management
-  const [subbed] = useState(false);
-  const [subStatus, setSubStatus] = useState('Subscribe');
+export class BlogFooter extends React.Component {
+  constructor (data) {
+    super(data);
+    this.seriesTitle = data.seriesTitle
+    this.seriesDescription = data.seriesDescription
 
-  const handleSubmit = (event) => {
-    setSubStatus('Subscribing...');
-    event.preventDefault();
+    this.state = {
+      email: '',
+      subbed: false,
+      subStatus: 'Subscribe'
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
-  return (
-    <section className="blog__footer">
-      <div className="container">
-        <div className="series-definition">
-          <h2 className="series-title">{seriesTitle}</h2>
-          <span className="series-description">{seriesDescription}</span>
+  handleChange(event) {
+    this.setState({email: event.target.value})
+  }
+
+  async handleSubmit(event) {
+    event.preventDefault()
+
+    const res = await axios.post('https://emailer.amongoose.com/subscribe', {
+      name: '',
+      email: this.state.email
+    })
+
+    if (res.status === 200) {
+      this.setState({subbed: true})
+    }
+  }
+
+  render() {
+    return (
+      <section className="blog__footer">
+        <div className="container">
+          <div className="series-definition">
+            <h2 className="series-title">{this.seriesTitle}</h2>
+            <span className="series-description">{this.seriesDescription}</span>
+          </div>
+          <div className="subscribeForm">
+            <h3>Get free updates via email</h3>
+            {
+              !this.state.subbed && <form onSubmit={this.handleSubmit}>
+                <input 
+                  type="email"
+                  name="email"
+                  placeholder="Email Address"
+                  value={this.state.email}
+                  onChange={this.handleChange}/>
+                <input type="submit" value={this.subStatus}/>
+              </form>
+            }
+            {
+              this.state.subbed && <div className="subscribeForm--success">
+                <span>Subscribed</span>
+              </div>
+            }
+          </div>
         </div>
-        <div className="subscribeForm">
-          <h3>Get free updates via email</h3>
-          {
-            !subbed && <form onSubmit={handleSubmit}>
-              <input type="email" name="email" placeholder="Email Address"></input>
-              <input type="submit" value={subStatus}></input>
-            </form>
-          }
-          {
-            subbed && <div className="subscribeForm--success">
-              <span>Subscribed</span>
-            </div>
-          }
-        </div>
-      </div>
-    </section>
-  )
+      </section>
+    )
+  }
 }
