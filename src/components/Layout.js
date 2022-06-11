f54eb660fb5d2e4a6110f99a2a8e5c9058275083
Joshua <joshua@grimmett.com.au>
2022-06-11 11:47:01 +1000

    Fontawesome link

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -23,6 +23,8 @@ const TemplateWrapper = ({ children, pageTitle, disableBackground, footer }) => 
         <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=DM+Serif+Display:ital@0;1&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"/>
         <script type="text/javascript" src="https://documentcloud.adobe.com/view-sdk/main.js"></script>
 
+        <script src="https://kit.fontawesome.com/b2926c9202.js" crossorigin="anonymous"></script>
+
         <link
           rel="apple-touch-icon"
           sizes="180x180"
